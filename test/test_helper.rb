78483a06b36ec79d3f9dc99c02d81fa7477261c2
adam <adam.kessler@lessonly.com>
2021-06-30 15:33:12 -0400

    add some tests, rack to mock, FABULOUS

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,12 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "llamakit"
 
 require "minitest/autorun"
+require "minitest/pride"
+require "byebug"
+require "rack/mock"
+
+def make_request(env)
+  req = Rack::Request.new(env)
+  yield(req) if block_given?
+  req
+end
